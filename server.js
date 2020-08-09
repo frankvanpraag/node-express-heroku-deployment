8e8b1bc9a07b9e6815bc53faff57af598927df50
frankvanpraag <16331016+frankvanpraag@users.noreply.github.com>
2020-08-09 21:59:57 +1000

    Added /date and /time

--- server.js
+++ server.js
@@ -5,5 +5,14 @@ app.get('/', (req, res) => {
   res.send("Hello World!");
 });
 
+app.get('/time', (req, res) => {
+  res.send("{ timestamp : " & Date.now() & " }");
+});
+
+app.get('/date', (req, res) => {
+  res.send("{ timestamp : " & Date.now() & " }");
+});
+
+
 const PORT = process.env.PORT || 3001;
 app.listen(PORT);
