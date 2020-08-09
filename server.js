acdccaa60dc50568bdb70c80d4a049604962ae97
JackZhang322 <69439470+JackZhang322@users.noreply.github.com>
2020-08-10 09:45:39 +1000

    added /time

--- server.js
+++ server.js
@@ -20,5 +20,20 @@ app.get('/date', (req, res) => {
   res.send({ info: 'Your name is ' + name, serverDate: niceDate, result: 'success', rc: 0 });
 });
 
+app.get('/time', (req, res) => {
+  console.log('Hello world...');
+  // Extract some parameters
+  var name = req.query.name;
+  if(name == '')
+    name = 'unknown';
+  var currentDate = new Date();
+  var hour = currentDate.getHours();
+  var minute = currentDate.getMinutes(); 
+  var second = currentDate.getSeconds();
+  var niceTime = hour + ':' + minute + ':' + second;
+
+  res.send({ info: 'Your name is ' + name, serverTime: niceTime, result: 'success', rc: 0 });
+});
+
 const PORT = process.env.PORT || 3001;
 app.listen(PORT);
