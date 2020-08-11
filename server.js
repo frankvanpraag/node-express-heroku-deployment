43417766de77c87bb2a056a4ddd20e7bf4b9a4fe
JackZhang322 <69439470+JackZhang322@users.noreply.github.com>
2020-08-11 20:30:38 +1000

    Update server.js
    added colour map

--- server.js
+++ server.js
@@ -1,39 +1,28 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send("Hello World, from Jack");
-});
+var colours = {
+  Jim = 'Blue',
+  Dwight = 'Orange',
+  Pam = 'Red',
+  Michael = 'Blue',
+  Toby = 'Grey',
+  Creed = 'Black',
+  Andy = 'Purple',
+  Unknown = 'Unknown'};
 
-app.get('/date', (req, res) => {
-  console.log('Hello world...');
-  // Extract some parameters
-  var name = req.query.name;
-  if(name == '')
-    name = 'unknown';
-  var currentDate = new Date();
-  var date = currentDate.getDate();
-  var month = currentDate.getMonth(); 
-  var year = currentDate.getFullYear();
-  var niceDate = date + '/' + month + '/' + year;
+app.get('/colour', (req, res) => {
+    console.log('Hello world...');
+    // Extract some parameters
+    var name = req.query.name;
+    if(name == '')
+      name = 'Unknown';
+    
+    var colour = colours.name;
+    res.send({ info: colour, result: 'success', rc: 0 });
 
-  res.send({ info: 'Your name is ' + name, serverDate: niceDate, result: 'success', rc: 0 });
-});
+  });
 
-app.get('/time', (req, res) => {
-  console.log('Hello world...');
-  // Extract some parameters
-  var name = req.query.name;
-  if(name == '')
-    name = 'unknown';
-  var currentDate = new Date();
-  var hour = currentDate.getHours();
-  var minute = currentDate.getMinutes(); 
-  var second = currentDate.getSeconds();
-  var niceTime = hour + ':' + minute + ':' + second;
-
-  res.send({ info: 'Your name is ' + name, serverTime: niceTime, result: 'success', rc: 0 });
-});
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT);
