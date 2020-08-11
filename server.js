5deae59ca5158a1d6ba3417cae64f30577bc7498
JackZhang322 <69439470+JackZhang322@users.noreply.github.com>
2020-08-11 21:37:03 +1000

    debugging

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const express = require('express');
 const app = express();
 
 var colours = {
-  Jim : 'Blue',
-  Dwight : 'Orange',
-  Pam : 'Red',
-  Michael : 'Blue',
-  Toby : 'Grey',
-  Creed : 'Black',
-  Andy : 'Purple',};
+  Jim : 'blue',
+  Dwight : 'orange',
+  Pam : 'red',
+  Michael : 'blue',
+  Toby : 'grey',
+  Creed : 'black',
+  Andy : 'purple',};
 
 
 app.get('/colour', (req, res) => {
@@ -16,14 +16,13 @@ app.get('/colour', (req, res) => {
     // Extract some parameters
     var name = req.query.name;
     if(name == '') {
-       name = 'Unknown'; 
+       name = 'unknown'; 
     }
 
-    if (Object.keys(colours).indexOf(name) > -1) {
-         var colour = colours.name;
-      } else {
-        var colour = 'unknown';
-      }
+    if (name in colours) {
+      var colour = colours.name;
+  } else {
+    var colour = 'unknown';
     
     res.send({ info: colour, result: 'success', rc: 0 });
 
