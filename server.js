059a12aaac649cc51af3dfa4a80f3284532d6d6b
JackZhang322 <69439470+JackZhang322@users.noreply.github.com>
2020-09-21 21:39:54 +1000

    added draft request for disable

--- server.js
+++ server.js
@@ -37,10 +37,50 @@ app.get('/disable', (req, res) => {
     var auth = req.query.auth;
   // Call SOTI here
   
+  
+  var http = require('http');
+var data = JSON.stringify({
+  'id': '2'
+});
+//Bricked Group: 'referenceId:dcacdec5-e9d2-43a8-bade-7baf7b19ccb7’
+var options = {
+  host: 's111720.mobicontrolcloud.com',
+  port: '443',
+  path: '/MobiControl/api/devices/' + devId + '/parentPath',
+  method: 'PUT',
+  headers: {
+    'Content-Type': 'application/json; charset=utf-8',
+    'Accept': 'application/json',
+    'Content-Length': data.length
+    'Authorization': 'Bearer ' + devId,
+    'newPath': 'referenceId:75e1cdac-030b-46f4-bd7d-316345ef0f1d'
+  }
+};
+
+var req = http.request(options, function(res) {
+  var msg = '';
+
+  res.setEncoding('utf8');
+  res.on('data', function(chunk) {
+    msg += chunk;
+  });
+  res.on('end', function() {
+    console.log(JSON.parse(msg));
+  });
+});
+
+req.write(data);
+req.end();
+  
+  //end soti call
+  
+  
+  
     res.send({ info: colour, result: 'success', rc: 0 });
 
   });
 
+//'referenceId:75e1cdac-030b-46f4-bd7d-316345ef0f1d' enable path
 app.get('/enable', (req, res) => {
 
     // Extract some parameters
