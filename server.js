c24dc7e9c6bb35a2b5937644c84d5ebb05578d59
frankvanpraag <16331016+frankvanpraag@users.noreply.github.com>
2020-09-24 12:13:40 +1000

    Update server.js
    Copied from https://raw.githubusercontent.com/JackZhang322/simple-nodejs-heroku/master/server.js

--- server.js
+++ server.js
@@ -1,24 +1,116 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send("Hello World!");
+var colours = {
+  Jim : 'blue',
+  Dwight : 'orange',
+  Pam : 'red',
+  Michael : 'blue',
+  Toby : 'grey',
+  Creed : 'black',
+  Andy : 'purple'};
+
+
+app.get('/colour', (req, res) => {
+  
+    // Extract some parameters
+    const name = req.query.name;
+    if(name == '') {
+       name = 'unknown'; 
+    }
+
+ 
+    if (name in colours) {
+      const colour = colours[name];
+    };  
+  if (!(name in colours)) {
+      const colour = 'unknown';
+    };  
+    res.send({ info: colour, result: 'success', rc: 0 });
+
+  });
+
+app.get('/disable', (req, res) => {
+  // Extract some parameters from qualtrics
+  const devId = req.query.devId;
+  const auth = req.query.auth;
+
+  
+  
+  // Call SOTI here
+  const https = require('https');
+
+  //build the mobicontrol request
+  const options = {
+    host: 's111720.mobicontrolcloud.com',
+    port: 443,
+    path: '/MobiControl/api/devices/' + devId + '/parentPath',
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+      'Authorization': 'Bearer ' + auth,
+      'newPath': 'referenceId:dcacdec5-e9d2-43a8-bade-7baf7b19ccb7'
+    }
+  };
+  
+  const req2 = https.request(options, function(res2) {
+    //dont really need callback but im not sure if i can remove it
+    res2.on('data', (d) => {
+      process.stdout.write(d);
+    });
+  });
+
+
+  req2.end();
+  
+  //end soti call
+  
+  
+  res.send({ Message: 'reached end of block'});
 });
 
-app.get('/date', (req, res) => {
-  console.log('Hello world...');
+// testing alternate code
+//const disreq = new XMLHttpRequest();
+//disreq.open('PUT', 'https://s111720.mobicontrolcloud.com/MobiControl/api/devices/' + devId + '/parentPath');
+//disreq.responseType = 'application/json';
+//disreq.send();
+
+
+app.get('/enable', (req, res) => {
   // Extract some parameters
-  var name = req.query.name;
-  if(name == '')
-    name = 'unknown';
-  var currentDate = new Date();
-  var date = currentDate.getDate();
-  var month = currentDate.getMonth(); 
-  var year = currentDate.getFullYear();
-  var niceDate = date + '/' + month + '/' + year;
+  const devId = req.query.devId;
+  const auth = req.query.auth;
+  const https = require('https');
 
-  res.send({ info: 'Your name is ' + name, serverDate: niceDate, result: 'success', rc: 0 });
+
+  const options = {
+    host: 's111720.mobicontrolcloud.com',
+    port: 443,
+    path: '/MobiControl/api/devices/' + devId + '/parentPath',
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+      'Authorization': 'Bearer ' + auth,
+      'newPath': 'referenceId:75e1cdac-030b-46f4-bd7d-316345ef0f1d'
+    }
+  }
+  
+  const req2 = https.request(options, function(res2) {
+    //dont really need callback but im not sure if i can remove it
+    res2.on('data', (d) => {
+      process.stdout.write(d);
+    });
+  });
+  
+  
+  req2.end();
+  res.send({
+    Message: 'reached end of block'}
+    );
 });
+
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT);
