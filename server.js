6f146273c036fd6a0ec9fa2b4eeda75e79945d1c
frankvanpraag <16331016+frankvanpraag@users.noreply.github.com>
2020-09-24 13:05:39 +1000

    Update server.js

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ app.get('/disable', (req, res) => {
       'Accept': 'application/json',
       'Authorization': 'Bearer ' + auth
     },
-    body: JSON.stringify("'referenceId:dcacdec5-e9d2-43a8-bade-7baf7b19ccb7'"),    
+    body: JSON.stringify('referenceId:dcacdec5-e9d2-43a8-bade-7baf7b19ccb7'),    
   };
   
   process.stdout.write(JSON.stringify(options));
